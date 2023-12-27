5c53bbda07ab0f8c7a56c54dd32fcdd3f0cb369d
marianesoares <marianesoaress1998@gmail.com>
2023-12-27 00:21:48 -0300

    feat: update dotenv info

--- config.ts
+++ config.ts
@@ -5,14 +5,12 @@ export function getConfig() {
   const username = process.env.API_USERNAME;
   const password = process.env.API_PASSWORD;
   const apiUrl = process.env.API_BASE_URL;
-  const token = process.env.API_TOKEN;
   const rabbitUrl = process.env.RABBITMQ_URL;
 
   return {
     username,
     password,
     apiUrl,
-    token,
     rabbitUrl
   };
 }
